2253b4be08b59239018136e75158e43cd536a23a
sit-tadano <j9344hcl@sit.ac.jp>
2021-07-25 02:34:22 +0900

    handle_followにusageなどを追加

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ token = json.load(token)
 from linebot.models.events import FollowEvent
 from linebot.models.send_messages import ImageSendMessage
 import json
-import datetime, random,pytz
+import datetime, random, pytz, re
 from MysqlManager import MysqlConnectorManager
 
 sqladdmin = open('../lib/sqladdmin.json','r')
@@ -43,6 +43,12 @@ dt_now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
 dt_h = dt_now.hour
 month = dt_now.month
 season = (month%12 + 3)//3
+# 取り込んだ時に送信する画像選択用の乱数
+def gen_random(min, max):
+    if random.random() >= 0.2:
+        return min
+    else:
+        return max
 
 # 友達登録時に送信するメッセージ(随時追加OK)
 @handler.add(FollowEvent)
@@ -94,6 +100,10 @@ def handle_message(event):
 def flagroute(event,result,CM):
     if result.flag == "ASKADDRESS":
         #messageがddd-ddd形式かチェックしてADDRESSに格納
+        if re.match(r"[0-9]{3}-[0-9]{4}",event.message):
+            CM.update_delete_contents(("UPDATE USER SET flag=%s where UserId = %s"),("FLAT",result.UserId))
+        else:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='郵便番号は ddd-dddd の形で送信してください！'))
         CM.update_delete_contents(("UPDATE USER SET flag=%s where UserId = %s"),("FLAT",result.UserId))
         #USAGEMessageを送る
     elif result.flag == "FLAT":
@@ -102,33 +112,62 @@ def flagroute(event,result,CM):
             #取込み予想の計算、メッセージへ　ScheduledTimeに
             CM.update_delete_contents(("UPDATE USER SET flag=%s　ScheduledTime=%s where UserId = %s"),("WaitTakeIn",ScheduledTime,result.UserId))
         elif event.message == "コレクション":
+            collection_items = CM.fetch_contents(("SELECT CollectionSum FROM USER ORDER BY %s")('ASC'))
+            for item_url in collection_items:
+                line_bot_api.broadcast(ImageSendMessage(original_content_url=item_url, preview_image_url=item_url))
             #DBからコレクションを取得しメッセージへ
             CM.update_delete_contents(("UPDATE USER SET flag=%s where UserId = %s"),("FLAT",result.UserId))
         elif event.message == "リマインド":
             #何時にする？とmessage
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='何時にする？'))
             CM.update_delete_contents(("UPDATE USER SET flag=%s where UserId = %s"),("WaitRemindTime",result.UserId))
         else:
             #USAGEメッセージを送信
+            usage = '「干した」と送信すると、\n'\
+                    '洗濯物を取り込む時間をお知らせします。\n'\
+                    '「コレクション」と送信すると\n'\
+                    '今までに取得した花の画像が見れます。\n'\
+                    '「リマインド」と送信すると\n'\
+                    'リマインドの時間を変更できます。\n'\
+                    'リマインドは洗濯物を干す時間を促すものです。'
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=usage))
             CM.update_delete_contents(("UPDATE USER SET flag=%s where UserId = %s"),("FLAT",result.UserId))
     elif result.flag == "WaitTakeIn":
         if event.message == "取り込んだ":
             #コレクションをランダムで選び、何が貰えたか教える
-            newCollectionSum = 0
+            if season == 1: # 冬の時
+                random_num = gen_random(128, 256)
+            elif season == 2: # 春の時
+                random_num = gen_random(2, 4)
+            elif season == 3: # 夏の時
+                random_num = gen_random(8, 16)
+            else: # 秋の時
+                random_num = gen_random(32, 64)
+
+            fetch_result = CM.fetch_contents(("SELECT * FROM Items WHERE ItemId=%s"),(random_num))
+            fetch_url = fetch_result['ImageUrl']
+            # コレクションに追加
+            newCollectionSum = fetch_result['ImageId']
+            # 画像送信
+            line_bot_api.broadcast(ImageSendMessage(original_content_url=fetch_url,preview_image_url=fetch_url))
             #コレクションidを加算して更新　newCollectionSum
             CM.update_delete_contents(("UPDATE USER SET flag=%s CollectionSum=%s where UserId = %s"),("FLAT",newCollectionSum,result.UserId))
         else :
             #USAGEを送る
-            Message = ""
+            usage = 'もし洗濯物を取り込んだら、「取り込んだ」と送信してください。'
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=usage))
     elif result.flag == "WaitRemindTime":
-        if event.message == "00:00":#だれか正規表現で時刻かどうかみて
-            remindTime = ""
+        if re.match(r'([01][0-9]|2[0-1]):[0-5][0-9]',event.message):#だれか正規表現で時刻かどうかみて
+            remindTime = event.message
             CM.update_delete_contents(("UPDATE USER SET flag=%s remindTime=%s where UserId = %s"),("FLAT",remindTime,result.UserId))
         else:
             #USAGEを送る
-            Message = "USAGE"
+            usage = "リマインド時刻は、\n「00:00」のように送信してください。"
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=usage))
     else:
         #USAGEを送る
-        Message = "USAGE" 
+        usage = "エラーが起きました！！"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=usage))
 
 if __name__ == "__main__":
     app.run()
