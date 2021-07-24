b7fd596ab6409fd49e84c6f10be584a2f1271c67
ryoga <56ryoga@gmail.com>
2021-07-24 15:25:21 +0900

    郵便番号の入力チェック追加

--- src/RequestWhetherApi.py
+++ src/RequestWhetherApi.py
@@ -1,22 +1,41 @@
 import requests
 import json
 
-def get_current_weather(postal_code):
-    API_KEY = "xxxxxxxxxxxxxxxxxxx"  # xxxに自分のAPI Keyを入力。
+def get_current_weather(input):
+    API_KEY = "0fedfd4841eac192a6d3f3819f1bd491"  # xxxに自分のAPI Keyを入力。
     api = "http://api.openweathermap.org/data/2.5/forecast?zip={code},jp&units=metric&lang=ja&APPID={key}"
-
-    url = api.format(code=postal_code, key=API_KEY)
+    url = api.format(code=input, key=API_KEY)
 
     # 気象情報を取得
-    response = requests.get(url).json()
-    # APIレスポンスの表示
-    jsonText = json.dumps(response, indent=2)
-    print(jsonText)
+    res = requests.get(url).json()
 
-    return response
-
+    if res['message'] != 'city not found':
+        return res
+    else:
+        #郵便番号検索で見つからない場合 => 緯度、経度で検索する
+        url = "http://geoapi.heartrails.com/api/json"
+        params = {
+            "method" : "searchByPostal",
+            "postal" : input
+        }
+        res = requests.get(url,params=params)
+        dict = json.loads(res.text)
+        longitude = dict["response"]["location"][0]["x"]
+        latitude = dict["response"]["location"][0]["y"]
+        url='http://api.openweathermap.org/data/2.5/forecast'
+        params={
+            "lon":longitude,
+            "lat":latitude,
+            "units":"metric",
+            "APPID":"0fedfd4841eac192a6d3f3819f1bd491"
+        }
+        res = requests.get(url,params=params)
+        return res
 
 if __name__ == "__main__":
-    # 住所を指定する。
-    postal_code = '100-0005'  #東京都千代田区(すべての住所に対応していないため一部地域ではレスポンスがnot foundになる)
-    response = get_current_weather(postal_code)
+    #郵便番号
+    postal_code = '771-1623'
+    dict = json.loads(get_current_weather(postal_code).text)
+    print(dict)
+
+
